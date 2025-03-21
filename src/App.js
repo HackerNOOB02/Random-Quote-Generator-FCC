714e7221e7623c1d2217996032e85e83ae5fb960
HackerNOOB02 <luciocorker48@gmail.com>
2025-03-21 02:50:20 +0100

    completed react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
 
 function App() {
+  const [quote, setQuote] = useState("Money is not the most important thing in the world. Love is. Fortunately, I love money.");
+  const [author, setAuthor] = useState("Jackie Mason");
+  const [randomNumber, setRandomNumber] = useState(0);
+  
+  const getRandomQuote = () => {
+    let randomInteger = Math.floor(quotesArray.length*Math.random());
+    setRandomNumber(randomInteger);
+    setQuote(quotesArray[randomInteger].quote);
+    setAuthor(quotesArray[randomInteger].author);
+    
+  }
+
+  const quotesArray = [{quote: "Hire me already. If not impressed, view my next project", author: "Lucio Corker"}, 
+    {quote: "When days are dark, friends are few", author: "Sipho Gumede"}, 
+    {quote: "Life is what happens to you while you're busy making other plans.", author: "John Lennon"},
+    {quote: "The only way to do great work is to love what you do.", author: "Steve Jobs"},
+    {quote: "You can't blame gravity for falling in love.", author: "Albert Einstein"},
+    {quote: "The only thing we have to fear is fear itself.", author: "Franklin D. Roosevelt"},
+    {quote: "Money makes the world go round.", author: "Unknown"},
+    {quote: "The only thing we know is that we know nothing.", author: "Socrates"},
+    {quote: "I think, therefore I am.", author: "René Descartes"}];
+
+  
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="quote-box">
+        <p id="text">"{quote}"</p>
+        <p id="author">-{author}</p>
+       <button id="new-quote" onClick={()=>getRandomQuote()}>Generate new Random Quote:</button>
+       <button id="twitter"><a id="tweet-quote" href={"https://twitter.com/intent/tweet"}>Tweet</a></button>
+       </div>
       </header>
     </div>
   );
 }
 
 export default App;
+    
+        
+       
